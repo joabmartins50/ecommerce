756781fa1d216b2cd019a89fc967b11c14e5c032
JoabMartins <41220595+joabmartins50@users.noreply.github.com>
2019-09-06 11:05:02 -0300

    autoload classes

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$Sql = new Hcode\DB\Sql();
+
+	$results = $Sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit9e6fbdd7fcc6ef4e1081c62e47228da9::$classMap;
 
